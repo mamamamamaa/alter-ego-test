32396b82fb4e1f05b41a7fbda8fc5d62a8c7ccdf
mamamamamaa <maks.denishchuk@gmail.com>
2023-02-23 16:38:13 +0200

    configure routing v1

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,25 @@
+import { Outlet, Route, Routes } from "react-router-dom";
+import { Suspense } from "react";
+
 export const App = () => {
-  return <></>;
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            Layout
+            <Suspense>
+              <Outlet />
+            </Suspense>
+          </>
+        }
+      >
+        <Route index element={<>Home page</>} />
+        <Route path="/news" element={<>News page</>} />
+        <Route path="/profile" element={<>User page</>} />
+        <Route path="*" element={<>Not found</>} />
+      </Route>
+    </Routes>
+  );
 };
